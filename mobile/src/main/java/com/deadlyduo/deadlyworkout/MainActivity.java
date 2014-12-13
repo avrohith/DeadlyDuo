5d19ac7785b8c492c8c3033971e075fc7b04886b
rohith <avrohith@gmail.com>
2014-12-13 15:03:19 -0500

    Mobile part

--- mobile/src/main/java/com/deadlyduo/deadlyworkout/MainActivity.java
+++ mobile/src/main/java/com/deadlyduo/deadlyworkout/MainActivity.java
@@ -1,17 +1,63 @@
 package com.deadlyduo.deadlyworkout;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.telephony.SmsManager;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    Spinner spinner;
+    Button button;
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = (EditText) findViewById(R.id.phone_number);
+        spinner = (Spinner) findViewById(R.id.option_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.options, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
+        button = (Button) findViewById(R.id.send_sms);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String phoneNumber = editText.getText().toString();
+                if(phoneNumber.equals("")){
+                    Toast.makeText(getBaseContext(), "No phone number",Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                SmsManager smsManager = SmsManager.getDefault();
+                smsManager.sendTextMessage(phoneNumber, null, "Hello", null, null);
+
+            }
+        });
     }
 
 
